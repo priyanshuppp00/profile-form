114a30a18bbad95bf42a07bc1e1e322c96a6a67c
priyanshuppp00 <priyanshugabgwar00@gmail.com>
2025-05-27 00:39:57 +0530

    client/src/utils/api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,19 +1,21 @@
 export const checkUsernameAvailability = async (username) => {
   const res = await fetch(
-    `http://localhost:5000/api/users/check-username/${username}`
+    `${import.meta.env.VITE_API_URL}/api/users/check-username/${username}`
   );
   return res.json();
 };
 
 export const fetchCountries = async () => {
-  const res = await fetch("http://localhost:5000/api/locations/countries");
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/api/locations/countries`
+  );
   if (!res.ok) throw new Error("Failed to fetch countries");
   return res.json();
 };
 
 export const fetchStates = async (country) => {
   const res = await fetch(
-    `http://localhost:5000/api/locations/states/${country}`
+    `${import.meta.env.VITE_API_URL}/api/locations/states/${country}`
   );
   if (!res.ok) throw new Error("Failed to fetch states");
   return res.json();
@@ -21,7 +23,7 @@ export const fetchStates = async (country) => {
 
 export const fetchCities = async (state) => {
   const res = await fetch(
-    `http://localhost:5000/api/locations/cities/${state}`
+    `${import.meta.env.VITE_API_URL}/api/locations/cities/${state}`
   );
   if (!res.ok) throw new Error("Failed to fetch cities");
   return res.json();
